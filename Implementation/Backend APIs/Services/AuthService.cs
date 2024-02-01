63cd6403fc068adb842a5b13db6cea5c0babc39b
Rana Muhammed Ali <99143413+RanaMkk@users.noreply.github.com>
2024-02-01 21:22:45 +0200

    Update AuthService.cs

--- Implementation/Backend APIs/Services/AuthService.cs
+++ Implementation/Backend APIs/Services/AuthService.cs
@@ -1,14 +1,27 @@
-﻿using Aqay_v2.Models;
+using Aqay_v2.Helpers;
+using Aqay_v2.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Aqay_v2.Services
 {
     public class AuthService : IAuthService
     {
+        //Service Attributes
         private readonly UserManager<User> _userManager;
-        public AuthService(UserManager<User> userManager)
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly JWT _jwt;
+
+        //Constructor
+        public AuthService(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IOptions<JWT> jwt)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
+            _jwt = jwt.Value;
         }
         public async Task<AuthModel> SignupAsync(SignupModel model)
         {
@@ -40,7 +53,51 @@ namespace Aqay_v2.Services
                 }
                 return new AuthModel { Message = errors };
             }
+            //Create user as a buyer by default
+            await _userManager.AddToRoleAsync(user, "Buyer");
+            var jwtSecurityToken = await CreateJwtToken(user);
+            return new AuthModel
+            {
+                Email = user.Email,
+                ExpiresOn = jwtSecurityToken.ValidTo,
+                IsAuthenticated = true,
+                Roles = new List<string> { "User" },
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                UserName = user.UserName
+            };
+        }
+        //Create a user token
+        private async Task<JwtSecurityToken> CreateJwtToken(User user)
+        {
+            //get rules and claims
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var roleClaims = new List<Claim>();
 
+            foreach (var role in roles)
+                roleClaims.Add(new Claim("roles", role));
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwt.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwt.Issuer,
+                audience: _jwt.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(_jwt.DurationInDays),
+                signingCredentials: signingCredentials);
+
+            return jwtSecurityToken;
         }
     }
 }
