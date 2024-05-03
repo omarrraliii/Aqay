c478d6bf77e2be29cd19df0b71c50f8dc44adf6c
Rana Muhammed Ali <99143413+RanaMkk@users.noreply.github.com>
2024-05-03 14:28:56 +0300

    Delete "old code" Implementation/Backend directory

--- Implementation/Backend/Dtos/CategoryUpdateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Aqay_v2.Dtos
-{
-    public class CategoryUpdateDto
-    {
-        public string Name { get; set; }
-        public string NewName { get; set; }
-    }
-}
--- Implementation/Backend/Services/IMailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Aqay_v2.Services
-{
-    public interface IMailService
-    {
-        Task SendEmailAsync(string mailTo, string subject, string body, IList<IFormFile> attachments = null);
-    }
-}
