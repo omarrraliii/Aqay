14105698610d32786c97df34ee390e4ba4a20065
omarrraliii <omaralitaha98@gmail.com>
2024-06-20 20:46:18 +0300

    restore branch

--- Implelentation/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace aqay_apis;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    //One to many relationship with products
-    public List<Product> Products{ get; set; }
-}
--- Implelentation/Services/IMailingService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace aqay_apis;
-
-public interface IMailingService
-{
-    Task SendEmailAsync(string email, string subject, string body,IList<IFormFile> attachments=null);
-}
