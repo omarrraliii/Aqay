97a7f5298069306d4b8ed13b01be45babe8ea71f
omarrraliii <omaralitaha98@gmail.com>
2024-06-27 23:53:54 +0300

    Merge WishListService into AqayBackend

--- Implelentation/obj/Debug/net8.0/aqay_apis.AssemblyInfo.cs
+++ Implelentation/obj/Debug/net8.0/aqay_apis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aqay_apis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46789fa19eb547066a3bc7d1b9175d4f3ce23d27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79a861216123b355c35d9b9612b5a6a35be3901")]
 [assembly: System.Reflection.AssemblyProductAttribute("aqay_apis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aqay_apis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
