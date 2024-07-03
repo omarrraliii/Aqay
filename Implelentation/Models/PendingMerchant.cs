826fda85afca5d80bd3410d849297e63597be6f4
ranamkk55@gmail.com <ranamkk55@gmail.com>
2024-07-03 22:17:42 +0300

    fix TRN validation

--- Implelentation/Models/PendingMerchant.cs
+++ Implelentation/Models/PendingMerchant.cs
@@ -6,7 +6,7 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public string Username { get; set; }
-        public string NATID { get; set; }
+        public string? NATID { get; set; }
         public string? TRN { get; set; }
         public string BrandName { get; set; }
         public string phoneNumber { get; set; }
