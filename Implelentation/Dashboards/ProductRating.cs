42248c73f75e74e57233d8e4bfb2aa963f020713
ranamkk55@gmail.com <ranamkk55@gmail.com>
2024-07-04 20:53:53 +0300

    dashboard edit

--- Implelentation/Dashboards/ProductRating.cs
+++ Implelentation/Dashboards/ProductRating.cs
@@ -3,7 +3,6 @@
     public class ProductRating
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
         public double AverageRating { get; set; }
     }
 }
