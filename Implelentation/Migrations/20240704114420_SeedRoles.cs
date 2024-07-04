6ad75ffc1c8a83151604bfb8395a1b2fd3f6e591
ranamkk55@gmail.com <ranamkk55@gmail.com>
2024-07-04 14:47:58 +0300

    Final database schema migrations

---
+++ Implelentation/Migrations/20240704114420_SeedRoles.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aqay_apis.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), "Consumer", "CONSUMER", Guid.NewGuid().ToString() }
+            );
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), "Owner", "OWNER", Guid.NewGuid().ToString() }
+            );
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), "Admin", "ADMIN", Guid.NewGuid().ToString() }
+            );
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), "Staff", "STAFF", Guid.NewGuid().ToString() }
+            );
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), "Guest", "GUEST", Guid.NewGuid().ToString() }
+            );
+        }
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Name",
+                keyValue: "Consumer"
+            );
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Name",
+                keyValue: "Owner"
+            );
+
+        }
+    }
+}
